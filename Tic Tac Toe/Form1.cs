dd3a91eaf839d2f57272aa9149189f03a3790a63
Nanook123 <joshallfordnanook@gmail.com>
2024-01-14 16:24:58 +0000

    TicTacToe

---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,25 @@
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CPUmove(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayerClickButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
